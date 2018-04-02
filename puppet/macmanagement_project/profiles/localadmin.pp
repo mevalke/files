81ab218eed182f8ed6bb148200c4308ea86d322d
miikka.valkeapaa <miikka1973@gmail.com>
2018-04-02 17:52:44 +0300

    adds hiera folder and completes the localadmin profile

--- puppet/macmanagement_project/profiles/localadmin.pp
+++ puppet/macmanagement_project/profiles/localadmin.pp
@@ -1,3 +1,11 @@
 class profile::localadmin {
-  include localadmin
+  $macadmins      = lookup(localadmin::macadmins)
+  $hide_macadmins = lookup(localadmin::hide_macadmins)
+  $linuxadmins    = lookup(localadmin::linuxadmins)
+   
+  class { 'localadmin':  
+    macadmins      => $macadmins,
+    linuxadmins    => $linuxadmins,
+    hide_macadmins => $hide_macadmins,
+  }
 }
